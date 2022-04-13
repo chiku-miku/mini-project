c4433a0c06e8c28716a9637dea236f5cfb16644b
chiku-miku <akash.8237723493.ac@gmail.com>
2022-04-13 23:59:31 +0530

    Login page update

--- login.php
+++ login.php
@@ -1,3 +1,57 @@
-<?php
-    echo 'Hello Shubham';
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Bootstrap Example</title>
+    <meta charset="utf-8">    
+    <?php include('common/header.php'); ?>
+    <link rel="stylesheet" href="asset/css/style.css">
+</head>
+<body class="login-body">
+    <!-- Menu Start -->
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="#">WebSiteName</a>
+            </div>
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="index.php">Home</a></li>
+                <li><a href="about">About</a></li>
+                <li><a href="contact.php">Contact</a></li>
+                <li><a href="signup.php">Signup</a></li>
+                <li><a href="login.php">Login</a></li>
+            </ul>
+        </div>
+    </nav>
+    <!-- End -->
+
+    <div class="container">
+        <div class="col-md-3"></div>
+        <div class="col-md-6">
+            <div class="panel panel-primary mt-30">
+                <div class="panel-heading">Login</div>
+                <div class="panel-body">
+                    <div class="panel-group">
+                        <form action="" method="post">
+                            <div class="form-group">
+                                <label for="email">Email</label>
+                                <input class="form-control" type="email" id="email" name="email" placeholder="Enter Your Email"> 
+                            </div>
+
+                            <div class="form-group">
+                                <label for="Password">Password</label>
+                                <input class="form-control" type="password" id="pass" name="pass" placeholder="Enter Your Password">
+                            </div>
+
+                            <div class="form-group">
+                                <div class="">
+                                    <button type="submit" class="btn btn-primary">Login</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
