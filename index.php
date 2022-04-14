1b1792ddde74763cbfcec1c0bca6afeaa3f662ff
chiku-miku <akash.8237723493.ac@gmail.com>
2022-04-14 22:37:21 +0530

    Menu added in index page

--- index.php
+++ index.php
@@ -8,22 +8,8 @@
 </head>
 <body class="login-body">
     <!-- Menu Start -->
-    <nav class="navbar navbar-default">
-        <div class="container-fluid">
-            <div class="navbar-header">
-                <a class="navbar-brand" href="#">WebSiteName</a>
-            </div>
-            <ul class="nav navbar-nav">
-                <li class=""><a href="index.php">Home</a></li>
-                <li><a href="about">About</a></li>
-                <li><a href="contact.php">Contact</a></li>
-                <li><a href="signup.php">Signup</a></li>
-                <li><a href="login.php">Login</a></li>
-            </ul>
-        </div>
-    </nav>
+    <?php include('common/menu.php'); ?>
     <!-- End -->
-    
     
 </body>
 </html>
