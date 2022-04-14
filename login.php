090ba9934b586d8fc78fdd8a6d05954a9a7990d3
chiku-miku <akash.8237723493.ac@gmail.com>
2022-04-14 22:05:26 +0530

    edit contact.php and login.php

--- login.php
+++ login.php
@@ -14,7 +14,7 @@
                 <a class="navbar-brand" href="#">WebSiteName</a>
             </div>
             <ul class="nav navbar-nav">
-                <li class="active"><a href="index.php">Home</a></li>
+                <li ><a href="index.php">Home</a></li>
                 <li><a href="about">About</a></li>
                 <li><a href="contact.php">Contact</a></li>
                 <li><a href="signup.php">Signup</a></li>
