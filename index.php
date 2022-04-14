f37643218884b8ba1549c3185102e208acf1c81f
chiku-miku <akash.8237723493.ac@gmail.com>
2022-04-14 22:26:42 +0530

    Menu Code Remove

--- index.php
+++ index.php
@@ -7,23 +7,6 @@
     <link rel="stylesheet" href="asset/css/style.css">
 </head>
 <body class="login-body">
-    <!-- Menu Start -->
-    <nav class="navbar navbar-default">
-        <div class="container-fluid">
-            <div class="navbar-header">
-                <a class="navbar-brand" href="#">WebSiteName</a>
-            </div>
-            <ul class="nav navbar-nav">
-                <li class=""><a href="index.php">Home</a></li>
-                <li><a href="about">About</a></li>
-                <li><a href="contact.php">Contact</a></li>
-                <li><a href="signup.php">Signup</a></li>
-                <li><a href="login.php">Login</a></li>
-            </ul>
-        </div>
-    </nav>
-    <!-- End -->
-    
-    
+    <!-- Menu -->
 </body>
 </html>
